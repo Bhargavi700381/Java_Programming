f1a5b91b7ea90f08807fc78c8e831e73997d25df
Bhargavi700381 <bargavi700@gmail.com>
2026-08-13 18:17:32 +0530

    Add FinalKeyword class to demonstrate final variables

---
+++ FinalKeyword.java
@@ -0,0 +1,8 @@
+class FinalKeyword  {
+    public static void main(String[] args) {
+           final int SPEED_LIMIT=80;
+           final int currentSpeed=75;
+        System.out.println("Speed Limit: " + SPEED_LIMIT + " km/h");
+        System.out.println("Current Speed: " + currentSpeed + " km/h");
+    }
+}
