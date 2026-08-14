66bc07e0f30040723f086c7f9600961695fa8996
Bhargavi700381 <bargavi700@gmail.com>
2026-08-14 18:55:25 +0530

    Clean up division.java by removing comments
    Removed comments explaining variable initialization and division results.

---
+++ division.java
@@ -0,0 +1,15 @@
+class dividion {
+    public static void main(String[] args) {
+        int num1 = 20;  
+        int num2 = 4;   
+        int num3 = 3;   
+        int quotient1 = num1 / num2;  
+        int quotient2 = num1 / num3;  
+        double quotient3 = num1 / num3;  
+        double quotient4 = (double) num1 / num3;  
+        System.out.println("Quotient1: " + quotient1);
+        System.out.println("Quotient2: " + quotient2);
+        System.out.println("Quotient3: " + quotient3);
+        System.out.println("Quotient4: " + quotient4);
+    }
+}
