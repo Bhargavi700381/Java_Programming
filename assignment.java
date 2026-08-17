8a7865c8b554590ee119ed3b9c0125eb33ca6356
Bhargavi700381 <bargavi700@gmail.com>
2026-08-17 21:37:55 +0530

    Add Java program to convert storage units

---
+++ assignment.java
@@ -0,0 +1,7 @@
+class assignment {
+    public static void main(String[] args) {
+        int totalStorage=1024000,conversionFactor=1024;
+       totalStorage/=conversionFactor;
+        System.out.println(totalStorage);  
+    }
+}
